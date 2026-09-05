1e38afad3783c9a1aa7c531461b9a776bc39b5d5
Zafar Kapadia <zafarx@hotmail.com>
2026-09-05 03:53:41 -0500

    Changes to be committed: 	modified:   data/chroma_db_market_news/9e67707e-54a6-4833-964b-7a73a0a46fe9/data_level0.bin 	modified:   data/chroma_db_market_news/9e67707e-54a6-4833-964b-7a73a0a46fe9/length.bin 	modified:   data/chroma_db_market_news/chroma.sqlite3 	modified:   data/chroma_ingest.log 	modified:   data/trading_signals.db 	modified:   jobs/run-ingest-articles.bat 	modified:   jobs/run-ingest-rsi.bat

--- jobs/run-ingest-rsi.bat
+++ jobs/run-ingest-rsi.bat
@@ -1,11 +1,11 @@
 @echo off
 :: Navigate to your project folder
-cd /d "C:\Users\zafar\Documents\Carnegie Mellon Course\examples\stock-analysis-mcp\jobs"
+cd /d "C:\Users\zafar\Documents\Carnegie Mellon Course\examples\stock-analysis-mcp-web\jobs"
 
 :: Force Python UTF-8 environment variable to prevent emoji crashes
 set PYTHONUTF8=1
 
 :: Call Python directly from your specific virtual environment and run the pipeline
-"..\.venvstockanalysis\Scripts\python.exe" download-rsi.py
+"..\.venvstockanalysisweb\Scripts\python.exe" download-rsi.py
 
 exit
